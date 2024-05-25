58f05408cff1be44c28c15ff694d049015136832
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-05-25 15:01:06 -0300

    Fix ui
    Fixed main style

--- src/pages/courses/Courses.tsx
+++ src/pages/courses/Courses.tsx
@@ -7,22 +7,25 @@ import imageCourse from "../../assets/imgs/curso.png";
 export const Courses = () => {
   return (
     <main id="courses">
-      <h1>Cursos</h1>
-      <div id="contentCourse">
-        <img src={imageCourse} alt="Imagem de Curos" />
-        <div id="contentText">
-          <h2>Seminário Carisma</h2>
-          <p>
-            Mais do que um seminário bíblico, o Carisma é uma escola que através
-            da Palavra revelada e do Espírito Santo traz um ensino prático e
-            vivo, empoderando você a viver o propósito pelo qual existe!
-            <span>Duração:</span>1 ano
-            <span>Turmas:</span>
-            Sábados | 9h às 18:00h
-          </p>
-          <a href="https://linktr.ee/carisma.braga?utm_source=linktree_profile_share&ltsid=dc42037f-4fa4-43bd-b72d-0f738a1103e5">
-            Saiba mais
-          </a>
+      <div className="container">
+        <h1>Cursos</h1>
+        <div id="contentCourse">
+          <img src={imageCourse} alt="Imagem de Curos" />
+          <div id="contentText">
+            <h2>Seminário Carisma</h2>
+            <p>
+              Mais do que um seminário bíblico, o Carisma é uma escola que
+              através da Palavra revelada e do Espírito Santo traz um ensino
+              prático e vivo, empoderando você a viver o propósito pelo qual
+              existe!
+              <span>Duração:</span>1 ano
+              <span>Turmas:</span>
+              Sábados | 9h às 18:00h
+            </p>
+            <a href="https://linktr.ee/carisma.braga?utm_source=linktree_profile_share&ltsid=dc42037f-4fa4-43bd-b72d-0f738a1103e5">
+              Saiba mais
+            </a>
+          </div>
         </div>
       </div>
     </main>
