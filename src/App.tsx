dc55fd4eb03c2a2921cbc03e12ba583954d462ff
Gabriel H. Finotti <finotti.dev@gmail.com>
2024-05-24 23:27:41 -0300

    Formatting cults page for mobile ready

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { Footer } from "./components/footer/Footer";
 import { Nav } from "./components/nav/Nav";
 import { About } from "./pages/about/About";
+import { Cults } from "./pages/cults/Cults";
 import { Home } from "./pages/home/Home";
 
 export const App = () => {
@@ -9,7 +10,8 @@ export const App = () => {
     <>
       <Nav></Nav>
       {/* <Home></Home> */}
-      <About></About>
+      {/* <About></About> */}
+      <Cults></Cults>
       <Footer></Footer>
     </>
   );
---
+++ src/pages/cults/Cults.tsx
@@ -0,0 +1,75 @@
+//Styles
+import "./Cults.scss";
+
+export const Cults = () => {
+  return (
+    <main id="cults">
+      <h1>Cultos</h1>
+      <p>
+        "Nenhum membro de um corpo pode subsistir sozinho, sem o resto do
+        corpo."
+        <br />
+        (1 Coríntios 12:21-22)
+      </p>
+      <p>
+        Terça
+        <span>
+          CULTO HOPE
+          <br />
+          ÀS 20H
+        </span>
+      </p>
+      <p>
+        Quarta
+        <span>
+          CULTO DE ORAÇÃO
+          <br />
+          12H00
+        </span>
+        <span>
+          CURA E LIBERTAÇÃO
+          <br />
+          14H ÀS 17H
+        </span>
+        <span>
+          CULTO FÉ
+          <br />
+          20H00
+        </span>
+      </p>
+      <p>
+        Sábado
+        <span>
+          CULTO ROCKET
+          <br />
+          18H00
+        </span>
+        <span>
+          CULTO LEGACY
+          <br />
+          20H30
+        </span>
+      </p>
+      <p>
+        Domingo
+        <span>
+          CULTO DE CELEBRAÇÃO DA FAMÍLIA
+          <br />
+          10H30 E 19H00
+        </span>
+        <span>
+          KIDS E LINK
+          <br />
+          10H30 E 18H00
+        </span>
+      </p>
+      <a
+        href="https://www.youtube.com/@LagoinhaBraga"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Assitir Online
+      </a>
+    </main>
+  );
+};
